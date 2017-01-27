093cea5bb8c41b3b35e2beb2cd64993dba6278ea
Paul <paulll.lucas@gmail.com>
2017-01-26 22:24:05 -0800

    Add open_sesame

---
+++ open_sesame.cljs
@@ -0,0 +1,15 @@
+#!/usr/bin/env lumo 
+(require '[cljs.nodejs :as nodejs])
+(def http (nodejs/require "http"))
+(def child-process (nodejs/require "child_process"))
+
+(.listen
+ (.createServer
+  http (fn [req res]
+         (.writeHead
+          res 200 (clj->js {"Content-Type" "text/plain"}))
+         (.execSync child-process "FOOBAR")
+         (.end res "Open sesame")))
+ 3000 "127.0.0.1")
+
+(println "Now listening on localhost:3000")
