cc9e8d00d4725a1ea5a27cba2a6001e40be67721
Paul <paulll.lucas@gmail.com>
2017-01-26 19:41:37 -0800

    Add http

---
+++ http.cljs
@@ -0,0 +1,15 @@
+#!/usr/bin/env lumo 
+(require '[cljs.nodejs :as nodejs])
+(def fs (nodejs/require "fs"))
+(def http (nodejs/require "http"))
+(def html (.readFileSync fs "index.html"))
+
+(.listen
+ (.createServer
+  http (fn [req res]
+         (.writeHead
+          res 200 (clj->js {"Content-Type" "text/html"}))
+         (.end res html)))
+ 3000 "127.0.0.1")
+
+(println "Now listening on localhost:3000")
