805739af8f9a21c6b851e4fc42367dcd2d1abf94
Paul <paulll.lucas@gmail.com>
2017-01-30 20:05:44 -0800

    Add ctest

---
+++ ctest/project.clj
@@ -0,0 +1,7 @@
+(defproject ctest "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]]
+  :npm {:dependencies [[source-map-support "0.4.0"]]}
+  :source-paths ["src" "target/classes"]
+  :clean-targets ["out" "release"]
+  :target-path "target")
---
+++ ctest/src/ctest/core.cljs
@@ -0,0 +1,5 @@
+(ns ctest.core
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+(enable-console-print!)
+(println "Hello world!")
