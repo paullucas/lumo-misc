67d214b6ffa96e12abdf97fa7a09ca7a091c51dd
Paul <paulll.lucas@gmail.com>
2017-02-08 15:44:53 -0800

    Remove v1 from ip.cljs

--- ip.cljs
+++ ip.cljs
@@ -2,31 +2,6 @@
 (require '[cljs.nodejs :as nodejs])
 (def https (nodejs/require "https"))
 
-;; v1
-
-;; (let [args (subvec (js->clj js/process.argv) 3)
-;;       url (if (> (count args) 0)
-;;             (str "https://ipinfo.io/" (first args) "/json")
-;;             "https://ipinfo.io/json")]
-;;   (.get https url
-;;         (fn [res]
-;;           (.on res "data"
-;;                (fn [json]
-;;                  (let [data (js->clj (.parse js/JSON json) :keywordize-keys true)]
-;;                    (dorun (map
-;;                            (fn [d]
-;;                              (println (str (:name d) ": " (:value d))))
-;;                            [{:name "IP Address" :value (:ip data)}
-;;                             {:name "Hostname" :value (:hostname data)}
-;;                             {:name "City" :value (:city data)}
-;;                             {:name "Region" :value (:region data)}
-;;                             {:name "Country" :value (:country data)}
-;;                             {:name "Location" :value (:loc data)}
-;;                             {:name "Organization" :value (:org data)}
-;;                             {:name "Postal" :value (:postal data)}]))))))))
-
-;; v2
-
 (defn get-url []
   (let [args (subvec (js->clj js/process.argv) 3)]
     (if (> (count args) 0)
