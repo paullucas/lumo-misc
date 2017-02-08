5b2ecfc33b00aeb5a6e5a4305d118fde776cddc3
Paul <paulll.lucas@gmail.com>
2017-02-08 01:37:24 -0800

    Refactor ip.cljs

--- ip.cljs
+++ ip.cljs
@@ -2,23 +2,56 @@
 (require '[cljs.nodejs :as nodejs])
 (def https (nodejs/require "https"))
 
-(let [args (subvec (js->clj js/process.argv) 3)
-      url (if (> (count args) 0)
-            (str "https://ipinfo.io/" (first args) "/json")
-            "https://ipinfo.io/json")]
-  (.get https url
-        (fn [res]
-          (.on res "data"
-               (fn [json]
-                 (let [data (js->clj (.parse js/JSON json) :keywordize-keys true)]
-                   (dorun (map
-                           (fn [d]
-                             (println (str (:name d) ": " (:value d))))
-                           [{:name "IP Address" :value (:ip data)}
-                            {:name "Hostname" :value (:hostname data)}
-                            {:name "City" :value (:city data)}
-                            {:name "Region" :value (:region data)}
-                            {:name "Country" :value (:country data)}
-                            {:name "Location" :value (:loc data)}
-                            {:name "Organization" :value (:org data)}
-                            {:name "Postal" :value (:postal data)}]))))))))
+;; v1
+
+;; (let [args (subvec (js->clj js/process.argv) 3)
+;;       url (if (> (count args) 0)
+;;             (str "https://ipinfo.io/" (first args) "/json")
+;;             "https://ipinfo.io/json")]
+;;   (.get https url
+;;         (fn [res]
+;;           (.on res "data"
+;;                (fn [json]
+;;                  (let [data (js->clj (.parse js/JSON json) :keywordize-keys true)]
+;;                    (dorun (map
+;;                            (fn [d]
+;;                              (println (str (:name d) ": " (:value d))))
+;;                            [{:name "IP Address" :value (:ip data)}
+;;                             {:name "Hostname" :value (:hostname data)}
+;;                             {:name "City" :value (:city data)}
+;;                             {:name "Region" :value (:region data)}
+;;                             {:name "Country" :value (:country data)}
+;;                             {:name "Location" :value (:loc data)}
+;;                             {:name "Organization" :value (:org data)}
+;;                             {:name "Postal" :value (:postal data)}]))))))))
+
+;; v2
+
+(defn get-url []
+  (let [args (subvec (js->clj js/process.argv) 3)]
+    (if (> (count args) 0)
+      (str "https://ipinfo.io/" (first args) "/json")
+      "https://ipinfo.io/json")))
+
+(defn create-template [json]
+  (let [data (js->clj (.parse js/JSON json) :keywordize-keys true)]
+    [{:name "IP Address" :value (:ip data)}
+     {:name "Hostname" :value (:hostname data)}
+     {:name "City" :value (:city data)}
+     {:name "Region" :value (:region data)}
+     {:name "Country" :value (:country data)}
+     {:name "Location" :value (:loc data)}
+     {:name "Organization" :value (:org data)}
+     {:name "Postal" :value (:postal data)}]))
+
+(defn print-datum [data]
+  (println (str (:name data) ": " (:value data))))
+
+(defn print-data [json]
+  (dorun
+   (map print-datum (create-template json))))
+
+(defn get-res [url]
+  (.get https url #(.on % "data" print-data)))
+
+(get-res (get-url))
