1624fd671d60b79df0eaaf19c098a7f40429fb07
Paul <paulll.lucas@gmail.com>
2017-04-18 16:20:33 -0700

    Add rethinktest

---
+++ rethinkdb/rethinktest.cljs
@@ -0,0 +1,18 @@
+#!/usr/bin/env lumo
+(require '[cljs.nodejs :as nodejs])
+(def r (nodejs/require "rethinkdb"))
+(def db-name "eventstate")
+
+(defn get-events []
+  (-> (.connect r (clj->js {:db db-name}))
+      (.then (fn [connection]
+               (-> r
+                   (.table "events")
+                   (.run connection))))
+      (.then (fn [cursor]
+               (.toArray cursor)))
+      (.then (fn [result]
+               (let [data (js->clj result :keywordize-keys true)]
+                 (println data))))))
+
+(get-events)
