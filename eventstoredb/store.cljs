a48ef8f7a648319e32bb531a9c6f0984fcf4929c
Paul <paulll.lucas@gmail.com>
2017-04-20 19:54:24 -0700

    Update eventstoredb/

--- eventstoredb/store.cljs
+++ eventstoredb/store.cljs
@@ -1,18 +1,15 @@
 (require '[cljs.nodejs :as nodejs])
-(def es-client (nodejs/require "eventstore-node"))
+(def client (nodejs/require "eventstore-node"))
 (def uuid (nodejs/require "uuid"))
 (def stream-name "testStream")
-(def es-connection (.createConnection es-client (clj->js {}) "tcp://localhost:1113"))
-(def conn-settings (clj->js {}))
+(def connection (.createConnection client (clj->js {}) "tcp://localhost:1113"))
 
-(.connect es-connection)
-(.once es-connection
+(.connect connection)
+(.once connection
        "connected"
        (fn [tcp-end-point]
-         (js/console.log (str "Connected to eventstore at "
-                              (.-host tcp-end-point)
-                              ":"
-                              (.-port tcp-end-point)))))
+         (js/console.log
+          (str "Connected to eventstore at " (.-host tcp-end-point) ":" (.-port tcp-end-point)))))
 
 (def event-id (.v4 uuid))
 (def event-data (clj->js
@@ -20,18 +17,18 @@
                   :b (.v4 uuid)}))
 
 
-(def event (.createJsonEventData es-client
+(def event (.createJsonEventData client
                                  event-id
                                  event-data
                                  nil
                                  "testEvent"))
 (js/console.log "Appending...")
 
-(.then (.appendToStream es-connection
+(.then (.appendToStream connection
                         stream-name
-                        (.-any (.-expectedVersion es-client))
+                        (.-any (.-expectedVersion client))
                         event)
        (fn [result]
          (js/console.log (str "Stored event:" event-id))
          (js/console.log "Look for it at: http://localhost:2113/web/index.html#/streams/testStream")
-         (.close es-connection)))
+         (.close connection)))
--- eventstoredb/subscribe.cljs
+++ eventstoredb/subscribe.cljs
@@ -1,12 +1,11 @@
 (require '[cljs.nodejs :as nodejs])
-(def es-client (nodejs/require "eventstore-node"))
-(def user-credentials (nodejs/require "eventstore-node"))
+(def client (nodejs/require "eventstore-node"))
 (def uuid (nodejs/require "uuid"))
 
-(def credentials (es-client.UserCredentials. "admin" "changeit"))
-(def es-connection (.createConnection es-client
-                                      (clj->js {})
-                                      (clj->js {:host "localhost" :port 1113})))
+(def credentials (client.UserCredentials. "admin" "changeit"))
+(def connection (.createConnection client
+                                   (clj->js {})
+                                   (clj->js {:host "localhost" :port 1113})))
 
 (defn subscription-dropped [subscription reason error]
   (if error
@@ -22,15 +21,15 @@
     (js/console.log (.-eventId original-event))
     (js/console.log (.-eventType original-event))))
 
-(.connect es-connection)
-(.once es-connection
+(.connect connection)
+(.once connection
        "connected"
        (fn [tcp-end-point]
          (js/console.log (str "Connected to eventstore at "
                               (.-host tcp-end-point)
                               ":"
                               (.-port tcp-end-point)))
-         (.subscribeToAllFrom es-connection
+         (.subscribeToAllFrom connection
                               nil
                               true
                               event-appeared
@@ -38,10 +37,10 @@
                               subscription-dropped
                               credentials)))
 
-(.on es-connection "error"
+(.on connection "error"
      (fn [err]
        (js/console.log (str "Error occurred on connection: " err))))
 
-(.on es-connection "closed"
+(.on connection "closed"
      (fn [reason]
        (js/console.log (str "Connection closed, reason: " reason))))
