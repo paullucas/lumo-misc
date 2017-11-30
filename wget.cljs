5bd63af7c7949236b150d20ab3d06b6d11fbd542
Paul <paulll.lucas@gmail.com>
2017-11-29 20:43:50 -0800

    Wget.cljs should use CLI args

--- wget.cljs
+++ wget.cljs
@@ -2,7 +2,7 @@
 (require '[lumo.core]
          '[child_process :refer [exec]])
 
-(def urls [])
+(def urls cljs.core/*command-line-args*)
 
 (defn wget [url]
   (exec (str "wget -nv " url) println))
